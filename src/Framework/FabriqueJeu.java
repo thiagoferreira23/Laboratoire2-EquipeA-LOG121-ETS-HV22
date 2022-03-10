dc148ce60511a4881bd8aa3c58fc569ad461131d
thiferreira23 <djthiaf@outlook.com>
2022-03-10 08:47:19 -0500

    Changed the class from abstract to regular class. Added method bodies.

--- src/Framework/FabriqueJeu.java
+++ src/Framework/FabriqueJeu.java
@@ -7,12 +7,24 @@ package Framework;
  * @version 1.0
  * @since 2022-02-24 9:26 a.m.
  */
-public abstract class FabriqueJeu {
-    public abstract void initialiserJeu(int nbJoueur, int nbDeParJoueur, int nbFaceDe, int nbTour, IStrategie regleJeu);
-    public abstract void creerJoueur(int nbJoueur);
-    public abstract void creerDe(int nbFaceDe, int nbDeParJoueur);
-    public abstract void jouer();
-    public final void creerJeuBunco(int nbJoueur, int nbDeParJoueur, int nbFaceDe, int nbTour, IStrategie regleJeu){
+public class FabriqueJeu {
+    public void initialiserJeu(int nbJoueur, int nbDeParJoueur, int nbFaceDe, int nbTour, IStrategie regleJeu) {
+
+    }
+
+    public void creerJoueur(int nbJoueur) {
+
+    }
+
+    public void creerDe(int nbFaceDe, int nbDeParJoueur) {
+
+    }
+
+    public void jouer() {
+
+    }
+
+    public final void creerJeuBunco(int nbJoueur, int nbDeParJoueur, int nbFaceDe, int nbTour, IStrategie regleJeu) {
         initialiserJeu(nbJoueur, nbDeParJoueur, nbFaceDe, nbTour, regleJeu);
         creerJoueur(nbJoueur);
         creerDe(nbFaceDe, nbDeParJoueur);
